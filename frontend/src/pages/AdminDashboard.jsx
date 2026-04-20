b4cee4e6d00498be7ac4f3963fc67e67bea63751
MaAs0706e <aswanthmadhav07@gmail.com>
2026-04-20 21:14:17 +0530

    just testing

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,2 +1,13 @@
 import { useState } from "react";
 
+function AdminDashboard(){
+
+    return(
+        <div>
+            <h1>Admin Dashboard</h1>
+            <p>Welcome to the admin dashboard!</p>
+        </div>
+    )
+}
+
+export default AdminDashboard;
